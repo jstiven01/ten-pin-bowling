cc3b32f3f0df5e18d7a956d08e0c820222784645
jstiven01 <stiven10@gmail.com>
2020-03-26 13:33:44 -0500

    Creating Player Object

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,7 @@
-p 'Hello world'
+# frozen_string_literal: true
+
+File.open('results.txt', 'r') do |f|
+  f.each_line do |line|
+    puts line
+  end
+end
---
+++ spec/spec_player.rb
@@ -0,0 +1,10 @@
+require_relative '../lib/player'
+
+RSpec.describe Player do
+  describe '#initialize' do
+    it 'Create player object with name as a parameter' do
+      player = Player.new('Jeff')
+      expect(player.name).to eq('Jeff')
+    end
+  end
+end
